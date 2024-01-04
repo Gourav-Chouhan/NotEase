3d5224672006201b81a2b407b3a17c956f6201c7
Gourav Chouhan <gg0uravgch0uhan@gmail.com>
2024-01-04 13:35:54 +0530

    updated service worker for caching issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,8 @@
-// service-worker.js
+const cacheName = "app-cache-v1"; // Change the cache version when updating assets
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("app-cache").then((cache) => {
+    caches.open(cacheName).then((cache) => {
       return cache.addAll([
         "/",
         "/index.html",
@@ -15,7 +16,21 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      // Try to fetch a fresh copy from the network
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200) {
+          return response;
+        }
+
+        caches.open(cacheName).then((cache) => {
+          // Update the cache with the new response
+          cache.put(event.request, networkResponse.clone());
+        });
+
+        return networkResponse;
+      });
+
+      return response || fetchPromise;
     })
   );
 });
